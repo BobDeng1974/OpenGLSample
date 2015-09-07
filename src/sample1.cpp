67c24a3f88a02c70dc46e236e319f9851803b0bc
holmes <hcqmaker@gmail.com>
2015-09-08 02:08:02 +0800

    add bmp reader

--- src/sample1.cpp
+++ src/sample1.cpp
@@ -9,75 +9,12 @@
 
 
 */
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#include "texture.h"
-
-
-//=========================================================
-Texture texture;
-double xpos = 0, ypos = 0;
-
-
-// 顶点坐标
-const GLfloat vers[] = {
-    -0.5,-0.5,0.5, 0.5,-0.5,0.5, 0.5,0.5,0.5, -0.5,0.5,0.5, // 正面 4个顶点
-    0.5,-0.5,-0.5, -0.5,-0.5,-0.5, -0.5,0.5,-0.5, 0.5,0.5,-0.5, // 背面 4个顶点
-
-    -0.5,0.5,0.5, 0.5,0.5,0.5, 0.5,0.5,-0.5, -0.5,0.5,-0.5, // 上面 4个顶点
-    -0.5,-0.5,-0.5, 0.5,-0.5,-0.5, 0.5,-0.5,0.5, -0.5,-0.5,0.5, // 下面 4个顶点
-
-    -0.5,-0.5,-0.5, -0.5,-0.5,0.5, -0.5,0.5,0.5, -0.5,0.5,-0.5, // 左面 4个顶点
-    0.5,-0.5,0.5, 0.5,-0.5,-0.5, 0.5,0.5,-0.5, 0.5,0.5,0.5, // 右面 4个顶点
-};
-
-// 法线
-const GLfloat nors[] = {
-    0,0,1, 0,0,1, 0,0,1, 0,0,1,
-    0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,
-    0,1,0, 0,1,0, 0,1,0, 0,1,0,
-    0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,
-    -1,0,0, -1,0,0, -1,0,0, -1,0,0,
-};
-
-// 纹理坐标
-const GLfloat texcoos[] = {
-    0,0, 1,0, 1,1, 0,1,
-    0,0, 1,0, 1,1, 0,1,
-    0,0, 1,0, 1,1, 0,1,
-    0,0, 1,0, 1,1, 0,1,
-    0,0, 1,0, 1,1, 0,1,
-    0,0, 1,0, 1,1, 0,1,
-};
-
-// 顶点颜色
-const GLfloat cors[] = {
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-    1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f,
-};
-
-// 顶点索引
-const GLint indices[] = {
-    0,1,2, 0,2,3,
-    4,5,6, 4,6,7,
-
-    8,9,10, 8,10,11,
-    12,13,14, 12,14,15,
-
-    16,17,18, 16,18,19,
-    20,21,22, 20,22,23,
-};
 
 // 设置灯光的颜色
 const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -91,9 +28,52 @@ const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
 const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat high_shininess[] = { 100.0f };
 
+struct BitmapHeader
+{
+    unsigned short type;
+    unsigned int size;
+    unsigned short reserved1;
+    unsigned short reserved2;
+    unsigned short offbits;
+
+};
+
+struct BitmapInfoHeader
+{
+    unsigned int size;
+    int width;
+    int height;
+    unsigned short planes;
+    unsigned short bitcount;
+    unsigned int compression;
+    unsigned int sizeimage;
+    int xpelspermeter;
+    int ypelspermeter;
+    unsigned int clrused;
+    unsigned int clrimportant;
+};
+
+unsigned int LoadBitmap(const char* filename)
+{
+    FILE* fp = fopen(filename, "rb");
+    if (!fp)
+        return 0;
+
+    BitmapHeader header;
+    BitmapInfoHeader infoheader;
+
+    fread(&header, sizeof(BitmapHeader), 1, fp);
+    fread(&infoheader, sizeof(BitmapInfoHeader), 1, fp);
+
+
+
+    fclose(fp);
+    return 0;
+}
 
 //----------------------------------------------------------------------------
 //
+/*
 int LoadGLTextures()
 {
     int Status = 0;
@@ -109,10 +89,10 @@ int LoadGLTextures()
     }
     return Status;
 }
-
+*/
 //----------------------------------------------------------------------------
 //
-GLchar* ReadShaderData(const char* filename, GLsizei &sz)
+GLchar* ReadShaderData1(const char* filename, GLsizei &sz)
 {
     FILE *fn = fopen(filename, "rb");
     if (fn == NULL)
@@ -131,11 +111,11 @@ GLchar* ReadShaderData(const char* filename, GLsizei &sz)
 
 //----------------------------------------------------------------------------
 //
-GLuint  buildShader(const char* filename, GLenum type)
+GLuint  buildShader1(const char* filename, GLenum type)
 {
     GLsizei sz = 0;
     GLint succ;
-    GLchar* vsSource = ReadShaderData(filename, sz);
+    GLchar* vsSource = ReadShaderData1(filename, sz);
     GLuint  vs = glCreateShader(type);
     glShaderSource(vs, 1, &vsSource, &sz);
     glCompileShader(vs);
@@ -154,12 +134,12 @@ GLuint  buildShader(const char* filename, GLenum type)
 
 //----------------------------------------------------------------------------
 //
-GLuint buildProgram(const char*vsfn, const char* frfn)
+GLuint buildProgram1(const char*vsfn, const char* frfn)
 {
     GLint linked;
     GLuint program = glCreateProgram();
-    GLuint vsshader = buildShader(vsfn, GL_VERTEX_SHADER);
-    GLuint frshader = buildShader(frfn, GL_FRAGMENT_SHADER);
+    GLuint vsshader = buildShader1(vsfn, GL_VERTEX_SHADER);
+    GLuint frshader = buildShader1(frfn, GL_FRAGMENT_SHADER);
     glAttachShader(program, vsshader);
     glAttachShader(program, frshader);
     glLinkProgram(program);
@@ -176,42 +156,32 @@ GLuint buildProgram(const char*vsfn, const char* frfn)
     return program;
 }
 
-//----------------------------------------------------------------------------
-//
-void test_draw()
+static void error_callback(int error, const char* description){fputs(description, stderr);}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-
-    const GLfloat vers[] = {
-         0.0f,0.0f,0.0f,
-         25.0f,0.0f,0.0f,
-         30.0f,30.0f,0.0f,
-         0.0f,25.0f,0.0f,
-    };
-    const GLfloat cors[] = {
-        1.0f,1.0f,1.0f,
-        1.0f,1.0f,1.0f,
-        1.0f,1.0f,1.0f,
-        1.0f,1.0f,1.0f,
-    };
-    const GLint indices[] = {
-        0,1,2,3
-    };
-
-    glPushMatrix();
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, vers);
-        glEnableClientState(GL_COLOR_ARRAY);
-        glColorPointer(3, GL_FLOAT, 0, cors);
-        glDrawElements(GL_QUADS, 4, GL_UNSIGNED_INT, indices);
-    glPopMatrix();
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-//----------------------------------------------------------------------------
-//
-static void init()
+static void mouse_callback(GLFWwindow* window, int button, int action, int mods){}
+static void cursorpos_callback(GLFWwindow *window, double x, double y){}
+
+int main(int argc, char *argv[])
 {
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    glfwInit();
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_callback);
+    glfwSetCursorPosCallback(window, cursorpos_callback);
+
     //buildProgram("Data/shader.vert", "Data/shader.frag");
-    LoadGLTextures();
+    //LoadGLTextures();
+
+    LoadBitmap("Data/np.bmp");
     glClearColor(0,0,0,1);      // 清理屏幕为黑色
     glEnable(GL_CULL_FACE);     // 启用面剔除
     glCullFace(GL_BACK);
@@ -223,15 +193,11 @@ static void init()
     glEnable(GL_COLOR_MATERIAL);// 启用材质模式
     glEnable(GL_LIGHTING);      // 打开灯光
 
-    //
     glEnable(GL_TEXTURE_2D);
     glEnable(GL_BLEND);
     glEnable(GL_ALPHA_TEST);
     glAlphaFunc(GL_GREATER ,0.9);//0.5可以换成任何在0~1之间的数
     glShadeModel(GL_SMOOTH);
-    //glClearDepth(1.0f);
-    //glDepthFunc(GL_LEQUAL);
-    //glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
     // 设置灯光
     glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);  // 设置环境光颜色
@@ -245,56 +211,6 @@ static void init()
     glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);  // 设置镜面反射的颜色
     glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);// 镜面指数 该值越小，表示材质越粗糙，点光源发射的光线照射到上面，也可以产生较大的亮点
 
-}
-
-//----------------------------------------------------------------------------
-//
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-}
-
-//----------------------------------------------------------------------------
-//
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
-//----------------------------------------------------------------------------
-//
-static void mouse_callback(GLFWwindow* window, int button, int action, int mods)
-{
-    if (button == GLFW_MOUSE_BUTTON_RIGHT)
-    {
-
-    }
-}
-
-//----------------------------------------------------------------------------
-//
-static void cursorpos_callback(GLFWwindow *window, double x, double y)
-{
-    xpos = x;
-    ypos = y;
-}
-
-//----------------------------------------------------------------------------
-//
-int main1(int argc, char *argv[])
-{
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    glfwInit();
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetMouseButtonCallback(window, mouse_callback);
-    glfwSetCursorPosCallback(window, cursorpos_callback);
-
-    init();
     //glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
     while (!glfwWindowShouldClose(window))
@@ -313,28 +229,16 @@ int main1(int argc, char *argv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glTranslatef(0.0f ,0.0f, -3.0f);
 
-        test_draw();
+        const GLfloat vers[] = { 0.0f,0.0f,0.0f, 25.0f,0.0f,0.0f, 30.0f,30.0f,0.0f, 0.0f,25.0f,0.0f, };
+        const GLfloat cors[] = { 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,1.0f, };
+        const GLint indices[] = {  0,1,2,3 };
 
-        float r  = 50 * (float)glfwGetTime();
         glPushMatrix();
-            glRotatef(r, 1.0f, 0.0f, 0.0f);
-            glRotatef(r, 0.0f, 1.0f, 0.0f);
-
-            glBindTexture(GL_TEXTURE_2D, texture.texID);
             glEnableClientState(GL_VERTEX_ARRAY);
             glVertexPointer(3, GL_FLOAT, 0, vers);
-
-            glEnableClientState(GL_NORMAL_ARRAY);
-            glNormalPointer(GL_FLOAT, 0, nors);
-
-            glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-            glTexCoordPointer(2, GL_FLOAT, 0, texcoos);
-
             glEnableClientState(GL_COLOR_ARRAY);
             glColorPointer(3, GL_FLOAT, 0, cors);
-
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, indices);
-
+            glDrawElements(GL_QUADS, 4, GL_UNSIGNED_INT, indices);
         glPopMatrix();
 
         glfwSwapBuffers(window);
@@ -346,28 +250,4 @@ int main1(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
-
-#include "fntloader.h"
-#include "ms3dloader.h"
-
-int main2(int argc, char *argv[])
-{
-#if 0
-
-    using namespace ms3d_space;
-    ms3d_model_t* model = create_ms3d_model();
-    load_ms3d_file(model, "Data/tin_open_lay.ms3d");
-    dump_ms3d_file(model, "Data/tin_open_lay.log");
-    delete_ms3d_model(model);
-#endif // 0
-
-#if 1
-    using namespace fnt_space;
-    FntFile file;
-    //file.dump();
-    file.loadFntFile("Data/fnt_arial.fnt");
-    int k = 0;
-#endif // 1
-
-}
 
